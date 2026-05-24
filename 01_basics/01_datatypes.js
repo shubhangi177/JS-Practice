99c73bc9281effdf82a52824d254fb01939ef9a5
shubhangi177 <shubhangii7777@gmail.com>
2026-05-24 17:22:40 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs, not browser(in node there is a different syntax(browser uses js))
+
+console.log(3
+    +3);//code readability should be high
+console.log("Shub");
+
+let name ="shub"
+let age = 22
+let isLoggedIn = false
+let state;
+
+//number =>2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined=>value not defined
+//symbol => unique
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
