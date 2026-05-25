1416450ef6b5b4bd1c215b7e68c457fec74b0db6
shubhangi177 <shubhangii7777@gmail.com>
2026-05-25 15:13:33 +0000

    datatypes summary

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -28,3 +28,24 @@ const myFunction = function(){
 }
 
 console.log(typeof myFunction);// Return type Object Function
+
+//++++++++++++++++++++++++++
+
+//Stack (Primitive), Heap(Non-Primitive)
+
+let teamName = "Dhol"
+let coachName = teamName
+coachName = "Soul"
+console.log(teamName);
+console.log(coachName);
+
+let userOne = {
+    email:"123@gmail.com",
+    upi: "user@111"
+}
+
+let userTwo = userOne
+
+userTwo.email = "shub@google.com"
+
+console.log(userOne.email);
