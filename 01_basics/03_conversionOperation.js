c114e89ea54acfb2353b486a10f28e31015d732d
shubhangi177 <shubhangii7777@gmail.com>
2026-05-25 14:18:23 +0000

    Conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -44,4 +44,39 @@ let stringNumber = String(n);
 console.log(typeof stringNumber); // Output: string
 console.log(stringNumber);// Output: 33
 
+//************Operations ******************/
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "Shub"
+let str2 = " kya kar rahi hai"
+
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+2); //12
+console.log(1+"2"); //12
+console.log("1"+2+2); //122
+console.log(1+2+"2") //32
+
+console.log(+true);
+console.log(+"");
+
+  
+
+
+
+
+
+
+
 
