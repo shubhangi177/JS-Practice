f4fcace6c7db882b431c7c420c552a613a085a5f
shubhangi177 <shubhangii7777@gmail.com>
2026-05-26 14:27:15 +0000

    math

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,31 @@
+const score = 999
+console.log(score)
+
+const balance = new Number(333)
+console.log(balance);
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(1))
+
+const decimalnumber = 23.923
+console.log(decimalnumber.toPrecision(4));
+
+const hundreds = 10000
+console.log(hundreds.toLocaleString('en-IN'))
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
