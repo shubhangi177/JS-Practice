219bf2a78dc1049a400387d844f2a452566610ba
shubhangi177 <shubhangii7777@gmail.com>
2026-06-10 15:38:03 +0000

    loops

---
+++ 05_iterations/one.js
@@ -0,0 +1,32 @@
+//for
+for (let i = 1; i < 10; i++) {
+    const element = i;
+    console.log(element);
+}
+
+let myArray = ['cindrella',"snow white", "rupunzal"]
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+}
+
+// break
+for(let i = 0; i <= 20; i++) {
+    if(i == 5){
+        console.log('Detected 5');
+        break
+    }
+    console.log(i);
+}
+
+//continue
+for(let i = 0; i <= 20; i++) {
+    if(i == 5){
+        console.log('Detected 5');
+        continue
+    }
+    console.log(i);
+}
+
+//while 
+let 
