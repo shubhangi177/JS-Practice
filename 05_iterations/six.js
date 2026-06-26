52a72bdfef846be218b3df805640ad312eda32b3
shubhangi177 <shubhangii7777@gmail.com>
2026-06-26 14:49:34 +0000

    forEach, filter

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -12,3 +12,18 @@ myNums.forEach((num) =>{
 })
 
 console.log(newNums);
+
+const books = [
+    {title:'Book 1', genre:'Fiction', publish:1964, edition: 2004},    
+    {title:'Book 2', genre:'Non Fiction', publish:1975, edition: 2009},    
+    {title:'Book 3', genre:'History', publish:1988, edition: 2012},    
+    {title:'Book 4', genre:'Science', publish:2000, edition: 2023},    
+    {title:'Book 5', genre:'History', publish:1944, edition: 2000},    
+]
+
+// const userBooks = books.filter((bk) => bk.genre == 'History')
+// console.log(userBooks);
+
+const userBooks = books.filter((bk)=> bk.publish<2004 && bk.genre == 'History')
+console.log(userBooks);
+
