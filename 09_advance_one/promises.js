1083dba0f77099729d1688b5dba5165410c18135
shubhangi177 <shubhangii7777@gmail.com>
2026-07-09 15:06:34 +0000

    promise

---
+++ 09_advance_one/promises.js
@@ -0,0 +1,7 @@
+const promiseOne = new Promise(function(resolve, reject){
+    //do an async task
+    //DB calls, cryptography, network
+    setTimeout(function(){
+        console.log('')
+    })
+})
