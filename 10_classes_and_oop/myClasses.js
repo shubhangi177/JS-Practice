a2b8f940df36d3cbc5dc9b4d1a4f7b9aac1817e5
shubhangi177 <shubhangii7777@gmail.com>
2026-07-17 01:31:35 +0000

    classes

--- 10_classes_and_oop/myClasses.js
+++ 10_classes_and_oop/myClasses.js
@@ -1,2 +1,37 @@
-//ES6
-class 
+// //ES6
+// class User{
+//     constructor(username, email, password){
+//         this.username = username;
+//         this.email = email
+//         this.password = password;
+//     }
+//     encryptPassword(){
+//         return `${this.password}abc`
+//     }
+//     changeUsername(){
+//         return `${this.username.toUpperCase()}`
+//     }
+// }
+
+// const user1 = new User("shub","shub@google.com","123")
+// console.log(user1.encryptPassword());
+// console.log(user1.changeUsername());
+
+
+//behind the scene
+function User(username, email, password){
+    this.username = username;
+    this.email = email
+    this.password = password;
+}
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+User.prototype.changePassword = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+const user1 = new User("shub","shub@google.com","123")
+console.log(user1.encryptPassword());
+console.log(user1.changePassword());
