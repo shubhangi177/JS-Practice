9e6796c2c228f88c2568b55d38c49eb4ea19532e
shubhangi177 <shubhangii7777@gmail.com>
2026-07-19 14:25:55 +0000

    Changing properties of a object

--- 10_classes_and_oop/mathpi.js
+++ 10_classes_and_oop/mathpi.js
@@ -1,1 +1,24 @@
-//Object.getOwnPropertyDescriptor(Math)
+const descripter1 = Object.getOwnPropertyDescriptor(Math,"PI")
+console.log(descripter1);
+
+console.log(Math.PI);
+Math.PI = 5
+console.log(Math.PI);
+
+const fan = {
+    model:"a1fan",
+    price: 1000,
+    isAvailable: true
+}
+console.log(Object.getOwnPropertyDescriptor(fan,"price"));
+
+Object.defineProperty(fan,'price',{
+    writable: false,
+    enumerable: false
+})
+console.log(Object.getOwnPropertyDescriptor(fan,"price"));
+//console.log(fan);
+for(let [key, value] of Object.entries(fan)){
+    console.log(`${key}:${value}`);
+    
+}
