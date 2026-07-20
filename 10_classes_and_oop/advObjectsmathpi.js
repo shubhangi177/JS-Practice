bd4cce98a7cbdca9a934781c7fc88cbff567f1a9
shubhangi177 <shubhangii7777@gmail.com>
2026-07-20 00:35:22 +0000

    Advance Objects

---
+++ 10_classes_and_oop/advObjectsmathpi.js
@@ -0,0 +1,29 @@
+const descripter1 = Object.getOwnPropertyDescriptor(Math,"PI")
+console.log(descripter1);
+
+console.log(Math.PI);
+Math.PI = 5
+console.log(Math.PI);
+
+const fan = {
+    model:"a1fan",
+    price: 1000,
+    isAvailable: true,
+
+    fanNahiChala: function(){
+        console.log('fan nahi chala');
+    }
+}
+// console.log(Object.getOwnPropertyDescriptor(fan,"price"));
+
+Object.defineProperty(fan,'price',{
+//     writable: false,
+    enumerable: false,
+})
+//console.log(Object.getOwnPropertyDescriptor(fan,"price"));
+//console.log(fan);
+
+for(let [key, value] of Object.entries(fan)){
+    if(typeof value !== "function")
+    console.log(`${key}:${value}`);
+}
