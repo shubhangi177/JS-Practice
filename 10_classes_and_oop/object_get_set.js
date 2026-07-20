d8f5ce59302f7ad2a86d0efc5ea13c223aa08ea8
shubhangi177 <shubhangii7777@gmail.com>
2026-07-20 00:37:11 +0000

    hey

---
+++ 10_classes_and_oop/object_get_set.js
@@ -0,0 +1,11 @@
+const User = {
+    _email = 'hsh@s.com',
+    _password = "234",
+
+    get email(){
+        return this._email.toUpperCase()
+    },
+    set email(value){
+        this._email = value
+    }
+}
